c9d7b129411ee81d8beb3480b188c9778ceebf3c
akshay <aksh6239san@gmail.com>
2024-07-10 15:48:07 +0530

    working sample game version1

--- arenafight/BattleGame.java
+++ arenafight/BattleGame.java
@@ -1,15 +1,21 @@
 import java.util.Random;
-import java.util.*;
+import java.util.Scanner;
 
 class Player {
+    private String name;
     private int health;
     private int strength;
     private int attack;
 
-    public Player(int health, int strength, int attack) {
+    public Player(String name, int health, int strength, int attack) {
+        this.name = name;
         this.health = health;
         this.strength = strength;
         this.attack = attack;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getHealth() {
@@ -34,6 +40,11 @@ class Player {
     public boolean isAlive() {
         return this.health > 0;
     }
+
+    @Override
+    public String toString() {
+        return name + "(health=" + health + ", strength=" + strength + ", attack=" + attack + ")";
+    }
 }
 
 class Arena {
@@ -54,40 +65,45 @@ class Arena {
     private void attack(Player attacker, Player defender) {
         int attackRoll = rollDice();
         int defendRoll = rollDice();
-
+        System.out.printf("%s get %d on rolling attacking dice\n",attacker.getName(),attackRoll);
+        System.out.printf("%s get %d on rolling defending dice\n",defender.getName(),defendRoll);
         int attackDamage = attacker.getAttack() * attackRoll;
         int defense = defender.getStrength() * defendRoll;
-
+        
         int damageDealt = attackDamage - defense;
         if (damageDealt > 0) {
             defender.reduceHealth(damageDealt);
         }
 
-        System.out.printf("%s attacks with %d damage. %s defends with %d. %s health is now %d.\n",
-                attacker.getClass().getSimpleName(), attackDamage, defender.getClass().getSimpleName(), defense, defender.getClass().getSimpleName(), defender.getHealth());
+        System.out.printf("%s attacks with %d damage. %s defends with %d. %s health is now %d.\n\n",
+                attacker.getName(), attackDamage, defender.getName(), defense, defender.getName(), defender.getHealth());
     }
 
     public void startBattle() {
         while (player1.isAlive() && player2.isAlive()) {
             if (player1.getHealth() <= player2.getHealth()) {
+                System.out.println(player1.getName() + "'s turn:");
                 attack(player1, player2);
                 if (!player2.isAlive()) {
-                    System.out.println("Player 1 wins!");
+                    System.out.println(player1.getName() + " wins!");
                     break;
                 }
+                System.out.println(player2.getName() + "'s turn:");
                 attack(player2, player1);
                 if (!player1.isAlive()) {
-                    System.out.println("Player 2 wins!");
+                    System.out.println(player2.getName() + " wins!");
                 }
             } else {
+                System.out.println(player2.getName() + "'s turn:");
                 attack(player2, player1);
                 if (!player1.isAlive()) {
-                    System.out.println("Player 2 wins!");
+                    System.out.println(player2.getName() + " wins!");
                     break;
                 }
+                System.out.println(player1.getName() + "'s turn:");
                 attack(player1, player2);
                 if (!player2.isAlive()) {
-                    System.out.println("Player 1 wins!");
+                    System.out.println(player1.getName() + " wins!");
                 }
             }
         }
@@ -97,18 +113,26 @@ class Arena {
 public class BattleGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the PlayerA Details in this format: Health(int) Strength(int)  Attact(int) ");
-        int health_of_A=scanner.nextInt();
-        int strength_of_A=scanner.nextInt();
-        int attack_of_A=scanner.nextInt();
-        Player playerA = new Player(health_of_A, strength_of_A, attack_of_A);
-        System.out.println("Enter the PlayerB Details in this format: Health(int) Strength(int)  Attact(int) ");
 
-        int health_of_B=scanner.nextInt();
-        int strength_of_B=scanner.nextInt();
-        int attack_of_B=scanner.nextInt();
-        Player playerB = new Player(health_of_B, strength_of_B, attack_of_B);
+        System.out.println("Enter Player A's name:");
+        String nameA = scanner.next();
+        System.out.println("Enter Player A's health, strength, and attack (space-separated integers):");
+        int healthA = scanner.nextInt();
+        int strengthA = scanner.nextInt();
+        int attackA = scanner.nextInt();
+        Player playerA = new Player(nameA, healthA, strengthA, attackA);
+
+        System.out.println("Enter Player B's name:");
+        String nameB = scanner.next();
+        System.out.println("Enter Player B's health, strength, and attack (space-separated integers):");
+        int healthB = scanner.nextInt();
+        int strengthB = scanner.nextInt();
+        int attackB = scanner.nextInt();
+        Player playerB = new Player(nameB, healthB, strengthB, attackB);
+
         Arena arena = new Arena(playerA, playerB);
         arena.startBattle();
+
+        scanner.close();
     }
 }
