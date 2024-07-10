e9603edcc04555ebeeb305e74e256a5ed315e9ae
akshay <aksh6239san@gmail.com>
2024-07-10 12:02:35 +0530

    added Arena,main,player class

--- main.java
+++ main.java
@@ -1,44 +1,9 @@
-import java.util.Random;
-
-public class Player {
-    private int health;
-    private int strength;
-    private int attack;
-
-    public Player(int health, int strength, int attack) {
-        this.health = health;
-        this.strength = strength;
-        this.attack = attack;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void reduceHealth(int damage) {
-        this.health -= damage;
-        if (this.health < 0) {
-            this.health = 0;
-        }
-    }
-
-    public boolean isAlive() {
-        return this.health > 0;
-    }
-}
-
-
-
 public class main {
     public static void main(String[] args) {
-       
+        Player playerA = new Player(50, 5, 10);
+        Player playerB = new Player(100, 10, 5);
+
+        Arena arena = new Arena(playerA, playerB);
+        arena.startBattle();
     }
 }
